403c389123c96e0c10b137b4e248b82134c8493b
sepans <sepans@sepans.com>
2015-03-11 16:35:38 -0400

    git script

--- rename/templates/git.sh
+++ rename/templates/git.sh
@@ -8,7 +8,13 @@ newName=$3
 
 git status
 
-#git checkout -b lens-rename
+git checkout -b lens-rename
 
-#git add .
-#git rm oldN
+git add .
+git rm $oldName.html
+git rm $oldName.css
+git rm test/$oldName.html
+
+git commit . -m 'renaming'
+
+git push origin lens-rename
